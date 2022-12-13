37597b00342425d1dfe7f268acaba1a29b9205a2
19-Andrey-91 <92575746+19-Andrey-91@users.noreply.github.com>
2022-12-13 20:13:25 +0300

    SpringBarrel is ready.

--- PinBall/Assets/Springs/SpringBarrel/SpringBarrel.cs
+++ PinBall/Assets/Springs/SpringBarrel/SpringBarrel.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpringBarrel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float springPower;
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        GameObject ball = collision.gameObject;
+        Rigidbody bodyBall = ball.GetComponent<Rigidbody>();
+        Vector3 forceDirection = ball.transform.position - transform.position;
+        bodyBall.AddForce(forceDirection * springPower, ForceMode.Impulse);
     }
 }
